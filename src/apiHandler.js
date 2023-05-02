174775980a19c93f52aec47718a79545f63006f4
Rutvik Barot <brutvik@systemintegration.in>
2023-05-02 11:49:18 +0530

    Add Suggestion page.

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -1,1 +1,2 @@
 export const url = 'https://dummyjson.com/products?limit=100';
+export const urlCategory = 'https://dummyjson.com/products/category/';
--- src/screens/products.js
+++ src/screens/products.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import Header from './header'
 import { Link, useLocation } from 'react-router-dom'
-import { Badge, BottomNavigation, BottomNavigationAction, Button, Dialog, Paper, Rating } from '@mui/material';
+import { Badge, BottomNavigation, BottomNavigationAction, Button, Dialog, Grid, Paper, Rating } from '@mui/material';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Swal from "sweetalert2";
+import { urlCategory } from '../apiHandler';
 
 function Products() {
     const location = useLocation();
@@ -14,6 +15,8 @@ function Products() {
     const [selectedImage, setSelectedImage] = useState(product.images[0]);
     const [cartItems, setCartItems] = useState([]);
     const [openDrawer, setOpenDrawer] = useState(false);
+    const [products, setProducts] = useState([])
+    const category = product.category;
 
     useEffect(() => {
         const rData = localStorage.getItem("cart");
@@ -21,8 +24,25 @@ function Products() {
         console.log('cart', JSON.parse(rData));
     }, [])
 
+    useEffect(() => {
+        // method for get data from API
+        const API = async () => {
+            const response = await fetch(urlCategory + category)
+            const json = await response.json()
+            setProducts(json.products)
+            // For check response please uncomment below line.
+            // console.log('response', json.products);
+        }
+        API();
+    }, [])
+
 
     const handleImageClick = (image) => {
+        setSelectedImage(image);
+        setImage(image)
+    }
+
+    const handleImageLoad = (image) => {
         setSelectedImage(image);
         setImage(image)
     }
@@ -100,6 +120,7 @@ function Products() {
                                         boxShadow: selectedImage === data ? '0 0 2px 2px aqua' : 'none'
                                     }}
                                         onClick={() => handleImageClick(data)}
+                                        onLoad={() => handleImageLoad(product.images[0])}
                                     />
                                 </div>
                             )
@@ -129,6 +150,41 @@ function Products() {
                     <Button disabled={cartItems.length > 0 ? false : true} style={{ fontSize: '1.5rem', borderRadius: '12px', marginTop: '10rem', marginLeft: '1rem', backgroundColor: '#337DEF', color: '#fcc729' }} onClick={() => handleRemoveFromCart(product)} >Remove from cart</Button>
                 </div>
             </div>
+            <div style={{ textAlign: 'center', display:'flex' }}>
+                <hr style={{ borderTop: '1px solid black', width: '30%', margin: 'auto' }} />
+                <span>Suggestion for you</span>
+                <hr style={{ borderTop: '1px solid black', width: '30%', margin: 'auto' }} />
+            </div>
+
+            {/* suggestion */}
+            <Grid container spacing={2} style={{marginBottom:'4rem'}}>
+                {products.map(product => (
+                    <Grid key={product.id} item lg={4} md={6} sm={12} xs={12}>
+                        <div style={{ display: 'flex', margin: '1rem 1rem', padding: '1rem 1rem' }}>
+                            <div className="img" style={{ backgroundColor: 'rgba(128,128,128, 0.4)' }}>
+                                <Link to='/product' state={product}>
+                                    <img src={product.thumbnail} alt="" style={{ height: '10rem', width: '10rem', cursor: 'pointer', padding: '1rem 1rem' }} />
+                                </Link>
+                            </div>
+                            <div style={{ marginLeft: '2rem' }}>
+                                <h2 style={{ margin: '0' }}>{product.title}</h2>
+                                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                                    <h4 style={{ color: 'green' }}>{product.discountPercentage}% Off</h4>
+                                    <Rating
+                                        name="simple-controlled"
+                                        value={product.rating}
+                                        readOnly
+                                        style={{ marginLeft: '1rem' }}
+                                    />
+                                </div>
+                                <div>
+                                    <p style={{ fontSize: '2rem', margin: '0' }}>${product.price}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                ))}
+            </Grid>
             {/* bottom part */}
             <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
                 <BottomNavigation
